802cc76832b1e912f6ad93b0fcd087f46e76c76d
tpeyruse <thomas.peyruse@gmail.com>
2020-08-12 14:43:22 +0000

    first commit

---
+++ lapin_driver/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'lapin_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tpeyruse',
+    maintainer_email='thomas.peyruse@gmail.com',
+    description='driver for biped robot',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'lapin_driver = lapin_driver.sensorLoop:main',
+        ],
+    },
+)
